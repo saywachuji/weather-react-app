5437ae634df63fd1dd8eb39148faf0c48d5de9eb
saywachuji <85375973+saywachuji@users.noreply.github.com>
2023-03-27 15:03:14 -0700

    Imported bootstrap

--- src/index.js
+++ src/index.js
@@ -1,18 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 import Weather from './Weather';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <div className="App">
+      <div className="container">
     <Weather />
-    
+    </div>
     </div>
   </React.StrictMode>
 );
 
-
-reportWebVitals();
