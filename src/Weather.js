989da6c419f49250afe997994022b23f6872220d
saywachuji <85375973+saywachuji@users.noreply.github.com>
2023-03-30 17:50:10 -0700

    Added axios

--- src/Weather.js
+++ src/Weather.js
@@ -1,81 +1,104 @@
 import "./Weather.css"
+import React,{ useState } from 'react'
+import axios from 'axios'
 
 export default function Weather(){
-    let weatherData = {
-        city : "Napa",
-        date: "Mon 10, 12:00",
-        description: "Cloudy",
-        imgUrl: "http://openweathermap.org/img/wn/02d@2x.png",
-        humidity: 10,
-        wind: 2
+
+const [loaded, setLoaded] = useState(false);
+const [weatherData, setWeatherData] = useState({});
+
+const handleSearch = (response) => {
+setWeatherData({
+    city: response.data.city,
+    temp: response.data.temperature.current,
+    humidity: response.data.temperature.humidity,
+    wind: response.data.wind,
+    icon: response.data.condition.icon_url,
+    description: response.data.condition.description,
+
+});
+setLoaded(true);
+
+};
+
+if (loaded){
+    return(
       
-      }
-      return(
-      
-      <div className = "Weather">
-      <div className="search-bar">
-              <form>
-                <input type="text" placeholder="Find city" className="city" />
-                <input type="submit" value="Search" className="search-button" />
-              </form>
-            </div>
-      
-            <div className="today-date-container">
-              <p className="today-date">{weatherData.date}</p>
-              <p>Last updated: {weatherData.date}</p>
-              
-            </div>
-      
-            <div className="main-city">
-              <h1>{weatherData.city}</h1>
-            </div>
-              <div className="row">
-                <div className="col-4">
-                  <ul className="list">
-                  <li>Humidity: <span>{weatherData.humidity}</span></li>
-                  <li>Wind: <span>{weatherData.wind}</span>km/h</li>
-                  </ul>
-                </div>
-                <div className="col-4">
-                  <strong><span className="main-temp">12°C</span></strong>
-                  <br />
-                  <img className="main-image" alt={weatherData.description} src={weatherData.imgUrl} />
-                  <br />
-                  <span className="weather-description">{weatherData.description}</span> 
-                </div>
-                <div className="col-4">
-                  <div className="unit-container">
-                    <span className="units"><a href="/" className="celsius">C°</a></span>
-                    <span className="units"><a href="/" className="farenheit">F°</a></span>
+        <div className = "Weather">
+        <div className="search-bar">
+                <form>
+                  <input type="text" placeholder="Find city" className="city" />
+                  <input type="submit" value="Search" className="search-button" />
+                </form>
+              </div>
+        
+              <div className="today-date-container">
+                <p className="today-date">12 Oct, 2023</p>
+                <p>Last updated: 12 Oct, 2023</p>
+                
+              </div>
+        
+              <div className="main-city">
+                <h1>{weatherData.city}</h1>
+              </div>
+                <div className="row">
+                  <div className="col-4">
+                    <ul className="list">
+                    <li>Humidity: <span>{weatherData.humidity}</span></li>
+                    <li>Wind: <span>{weatherData.wind}</span>km/h</li>
+                    </ul>
+                  </div>
+                  <div className="col-4">
+                    <strong><span className="main-temp">{weatherData.temp}°C</span></strong>
+                    <br />
+                    <img className="main-image" alt={weatherData.description} src={weatherData.icon}/>
+                    <br />
+                    <span className="weather-description text-capitalize">{weatherData.description}</span> 
+                  </div>
+                  <div className="col-4">
+                    <div className="unit-container">
+                      <span className="units"><a href="/" className="celsius">C°</a></span>
+                      <span className="units"><a href="/" className="farenheit">F°</a></span>
+                    </div>
+                  </div>
+                
+                
+              </div>
+            
+              <button>
+                Current Location
+              </button>
+            
+              <h3>Forecast</h3>
+              <div className="forecast-container">
+                <div className="row">
+                  <div className="col">
+                    <div className="days">Mon</div>
+                   <img alt="{weatherData.description}"src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/clear-sky-day.png" width="70px" />
+                    <div className="degrees">
+                    <span>16°</span><span className="min-temp">12°</span>
+                  </div>
                   </div>
                 </div>
-              
-              
-            </div>
-          
-            <button>
-              Current Location
-            </button>
-          
-            <h3>Forecast</h3>
-            <div className="forecast-container">
-              <div className="row">
-                <div className="col">
-                  <div className="days">Mon</div>
-                 <img alt="{weatherData.description}"src={weatherData.imgUrl} width="70px" />
-                  <div className="degrees">
-                  <span>16°</span><span className="min-temp">12°</span>
-                </div>
-                </div>
               </div>
-            </div>
+          
+              <footer>
+              <div className="end">
+                <small className="end-text"><a href="https://github.com/saywachuji/weather-react-app" rel="noreferrer" target="_blank">Open source code</a> by Saywa Chuji</small>
+              </div>
+             </footer>
         
-            <footer>
-            <div className="end">
-              <small className="end-text"><a href="https://github.com/saywachuji/weather-react-app" rel="noreferrer" target="_blank">Open source code</a> by Saywa Chuji</small>
             </div>
-           </footer>
+        );
+} else {
+    let key = "c635taf5ao3b501623e4fa7bf7fc0f02";
+    let units = "metric";
+    let city = "Napa"
+    const apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${key}&units=${units}`
+    
+    axios.get(apiUrl).then(handleSearch)
+    return "Loading..."
+}
+   
       
-          </div>
-      );
 }
